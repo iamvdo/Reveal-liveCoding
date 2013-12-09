99a393d3c56494bc40e6ffd3410597afbc6beea2
Thomas ZILLIOX <thomas@zilliox.me>
2013-12-09 17:23:45 +0100

    Improve variable names & syntaxe consistency

--- live-coding.js
+++ live-coding.js
@@ -1,17 +1,17 @@
 /**
-* LiveCoding 0.1 for Reveal.js
-* Vincent De Oliveira
-*/
+ * LiveCoding 0.2 for Reveal.js
+ * Vincent De Oliveira
+ */
 var LiveCoding = (function() {
 	
 	// All <code> with ".liveCoding" class
-	var allCode = document.querySelectorAll('code.liveCoding');
+	var codeElementList = document.querySelectorAll('code.liveCoding');
 
-	for(var i = 0; i < allCode.length; i++){
-		update(allCode[i]);
+	for (var i = 0; i < codeElementList.length; i++) {
+		update(codeElementList[i]);
 
-		// update when keyup
-		allCode[i].addEventListener('keyup', function(){
+		// update when keyUp
+		codeElementList[i].addEventListener('keyup', function() {
 			update(this);
 		});
 	}
@@ -19,52 +19,64 @@ var LiveCoding = (function() {
 	/**
 	 * Update
 	 */
-	function update(code){
+	function update(codeElement) {
 
-		var id = code.attributes.getNamedItem('data-livecoding-id').nodeValue;
-		var val = code.textContent;
+		var demoElementId = codeElement.attributes.getNamedItem('data-livecoding-id').nodeValue;
+		var demoElement = document.getElementById(demoElementId);
 
 		// highlight.js and prism.js
-		var isCSS = (code.classList.contains('css') || code.classList.contains('language-css'));
-		var isMarkup = (code.classList.contains('xml') || code.classList.contains('language-markup') || code.classList.contains('language-xml') || code.classList.contains('language-html') || code.classList.contains('language-svg'));
-	
+		var isCSS = hasAtLeastOneClass(codeElement, ['css', 'language-css']);
+		var isMarkup = hasAtLeastOneClass(codeElement, ['xml', 'language-markup', 'language-xml', 'language-html', 'language-svg']);
+
 		// if it's CSS
-		if(isCSS){
+		if (isCSS) {
+			var cssRules = codeElement.textContent;
 
 			// if PrefixFree is here
-			if(typeof PrefixFree !== "undefined"){
+			if (typeof PrefixFree !== "undefined") {
 				// prefix code
-				val = PrefixFree.prefixCSS(val);	
+				cssRules = PrefixFree.prefixCSS(cssRules);
 			}
-			
-			// cleanup
-			val = val.replace(/^\s+/g,'').replace(/\s+$/g,'');
-			var reg = /(\{|\})/g;
-			val = val.split(reg);
-			for(var i = 0; i < val.length - 1; i+=4){
-				var selectors = val[i].split(',');
-				for(var j = 0; j < selectors.length; j++){
-					selectors[j] = '#' + id + ' ' + selectors[j];
-				}
-				val[i] = selectors.join(',');
-			}
-			val = val.join('');
 
-			// if <style id="liveCoding_9999"> exists, replace content
-			if( document.getElementById('liveCoding_' + id) != null ){
-				document.getElementById('liveCoding_' + id).innerHTML = val;
-			} else {
-			// else, create it	
-				var style = document.createElement('style');
-				style.setAttribute('id','liveCoding_' + id);
-				style.innerHTML = val;
-				document.body.appendChild(style);
+			// cleanup
+			cssRules = cssRules.replace(/^\s+/g,'').replace(/\s+$/g,'');
+			var reg = /(\{|\})/g;
+			cssRules = cssRules.split(reg);
+			for(var i = 0; i < cssRules.length - 1; i+=4){
+				var selectors = cssRules[i].split(',');
+				for(var j = 0; j < selectors.length; j++){
+					selectors[j] = '#' + demoElementId + ' ' + selectors[j];
+				}
+				cssRules[i] = selectors.join(',');
 			}
+			cssRules = cssRules.join('');
+
+			// if <style id="liveCoding_9999"> doesn't exist, create it
+			var styleElement = document.getElementById('liveCoding_' + demoElementId);
+			if( styleElement === null ){
+				styleElement = document.createElement('style');
+				styleElement.setAttribute('id','liveCoding_' + demoElementId);
+				insertAfter(demoElement, styleElement);
+			}
+			styleElement.innerHTML = cssRules;
 
 		// else, if it's markup (HTML, SVG, XML...)
-		}else if(isMarkup){
+		} else if(isMarkup) {
 			// replace content 
-			document.getElementById(id).innerHTML = val;
+			demoElement.innerHTML = codeElement.innerHTML;
+		}
+		
+		function hasAtLeastOneClass(element, classList) {
+			for (var i=0; i<classList.length; i++) {
+				if (element.classList.contains(classList[i])) {
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		function insertAfter(referenceNode, newNode) {
+			referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 		}
 	}
 
